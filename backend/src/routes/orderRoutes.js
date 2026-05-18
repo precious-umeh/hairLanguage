4bf83c1ab7ee78f4a940b2a06bfaa801c7f2be47
Precious Umeh <preciouschukwuanugoumeh@gmail.com>
2026-05-18 11:07:45 +0100

    modify order-routes

--- backend/src/routes/orderRoutes.js
+++ backend/src/routes/orderRoutes.js
@@ -14,15 +14,14 @@ import { authMiddleware } from "../middlewares/authMiddleware.js";
 import adminMiddleWare from "../middlewares/adminMiddleware.js";
 
 const router = express.Router();
+// User Routes
+router.get("/my-orders", authMiddleware, getUserOrders);
+router.patch("/:id/hide", authMiddleware, softDeleteOrder);
 
 // Public/Guest Route
 router.post("/create-order", optionalAuth, createOrder);
 router.post("/lookup", guestOrderLookup);
 router.get("/:id", optionalAuth, getOrderById);
-
-// User Routes
-router.get("/my-orders", authMiddleware, getUserOrders);
-router.patch("/:id/hide", authMiddleware, softDeleteOrder);
 
 // Admin Routes
 router.get("/admin/all", authMiddleware, adminMiddleWare, getAllOrders);
