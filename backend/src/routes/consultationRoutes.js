cfde9c90a220b9c3f9d9c4820b983eebfaad8282
Precious Umeh <preciouschukwuanugoumeh@gmail.com>
2026-05-18 11:37:46 +0100

    modify consultation/contact-messages routes

--- backend/src/routes/consultationRoutes.js
+++ backend/src/routes/consultationRoutes.js
@@ -10,15 +10,31 @@ import {
 } from "../controllers/consultationController.js";
 import { emailLimiter } from "../middlewares/emailRateLimiter.js";
 import { authMiddleware } from "../middlewares/authMiddleware.js";
+import adminMiddleWare from "../middlewares/adminMiddleware.js";
 
 const router = express.Router();
 
 router.post("/consultations", emailLimiter, sendBookings);
-router.get("/consultations", receiveBookings);
+router.get("/consultations", authMiddleware, adminMiddleWare, receiveBookings);
 router.get("/my-consultations", authMiddleware, getMyBookings);
-router.post("/consultations/:id/reply", replyToBooking);
-router.patch("/consultations/:id", updateBookingStatus);
+router.post(
+  "/consultations/:id/reply",
+  authMiddleware,
+  adminMiddleWare,
+  replyToBooking,
+);
+router.patch(
+  "/consultations/:id",
+  authMiddleware,
+  adminMiddleWare,
+  updateBookingStatus,
+);
 router.patch("/my-consultations/:id/hide", authMiddleware, softDeleteBooking);
-router.delete("/consultations/:id", deleteBookings);
+router.delete(
+  "/consultations/:id",
+  authMiddleware,
+  adminMiddleWare,
+  deleteBookings,
+);
 
 export default router;
--- backend/src/routes/contactMessageRoutes.js
+++ backend/src/routes/contactMessageRoutes.js
@@ -7,13 +7,25 @@ import {
   updateMessageStatus,
 } from "../controllers/contactMessageController.js";
 import { emailLimiter } from "../middlewares/emailRateLimiter.js";
+import { authMiddleware } from "../middlewares/authMiddleware.js";
+import adminMiddleWare from "../middlewares/adminMiddleware.js";
 
 const router = express.Router();
 
 router.post("/contact", emailLimiter, createMessage);
-router.get("/contact", getMessages);
-router.post("/contact/:id/reply", replyToMessage);
-router.patch("/contact/:id", updateMessageStatus);
-router.delete("/contact/:id", deleteMessage);
+router.get("/contact", authMiddleware, adminMiddleWare, getMessages);
+router.post(
+  "/contact/:id/reply",
+  authMiddleware,
+  adminMiddleWare,
+  replyToMessage,
+);
+router.patch(
+  "/contact/:id",
+  authMiddleware,
+  adminMiddleWare,
+  updateMessageStatus,
+);
+router.delete("/contact/:id", authMiddleware, adminMiddleWare, deleteMessage);
 
 export default router;
