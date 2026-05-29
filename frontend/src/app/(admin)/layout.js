1e98e4978ba6a9995c1efb0fc0cd27a6faa87cc5
Precious Umeh <preciouschukwuanugoumeh@gmail.com>
2026-05-29 17:21:44 +0100

    wrap admin-search-provider in layout.js

--- frontend/src/app/(admin)/layout.js
+++ frontend/src/app/(admin)/layout.js
@@ -11,6 +11,7 @@ import { useAuth } from "@/providers/admin/auth-provider";
 import { useRouter, usePathname } from "next/navigation";
 import toast, { Toaster } from "react-hot-toast";
 import { Loader2 } from "lucide-react";
+import { AdminSearchProvider } from "@/providers/admin/admin-search-provider";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -81,12 +82,14 @@ export default function AdminLayout({ children }) {
             <AdminMobNav isOpen={isMobNavOpen} setIsOpen={setIsMobNavOpen} />
 
             <div className="flex-1 flex flex-col min-w-0">
-              <Topbar
-                isOpen={isMobNavOpen}
-                toggleNav={() => setIsMobNavOpen(!isMobNavOpen)}
-              />
+              <AdminSearchProvider>
+                <Topbar
+                  isOpen={isMobNavOpen}
+                  toggleNav={() => setIsMobNavOpen(!isMobNavOpen)}
+                />
 
-              <div className="flex-1 p-8 overflow-y-auto ">{children}</div>
+                <div className="flex-1 p-8 overflow-y-auto ">{children}</div>
+              </AdminSearchProvider>
             </div>
           </div>
         </NotificationProvider>
