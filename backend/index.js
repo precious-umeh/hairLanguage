f3fc2cc2fbe83f0eb96f97037ccf40021d27fef7
Precious Umeh <preciouschukwuanugoumeh@gmail.com>
2026-06-03 21:59:30 +0100

    modify index file

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,12 @@
+import "./src/config/env.js";
 import server from "./src/server.js";
 import express from "express";
-import dotenv from "dotenv";
+// import dotenv from "dotenv";
 import { fileURLToPath } from "url";
 import path from "path";
+import { env } from "./src/config/env.js";
 
-dotenv.config();
+// dotenv.config();
 
 const port = process.env.PORT || 5500;
 const filename = fileURLToPath(import.meta.url);
@@ -12,6 +14,7 @@ const directoryName = path.dirname(filename);
 
 server.use("/uploads", express.static(path.join(directoryName, "uploads")));
 
-server.listen(port, () =>
-  console.log(`Server running at http://127.0.0.1:${port}`),
+server.listen(env.port, () =>
+  // console.log(`Server running at http://127.0.0.1:${port}`),
+  console.log(`Server running on port ${env.port}`),
 );
